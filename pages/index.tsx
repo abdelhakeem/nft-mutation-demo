4649182f08fc8ad3009f8ae878a542545a87893a
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2022-07-13 05:44:27 +0200

    Add Status component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,23 @@
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import type { NextPage } from "next";
 import NFTPicker from "../components/NFTPicker";
+import Status from "../components/Status";
 import useNFTs from "../hooks/useNFTs";
 
 const Home: NextPage = () => {
+  const wallet = useAnchorWallet();
   const { data, error } = useNFTs();
 
+  if (!wallet) {
+    return <Status type="info" msg="Please connect your wallet to proceed" />
+  }
+
   if (error) {
-    return (
-      <div className="alert alert-error shadow-lg">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="stroke-current flex-shrink-0 h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>Could not fetch NFTs: {error.message}</span>
-        </div>
-      </div>
-    );
+    return <Status type="error" msg={error.message} />
   }
 
   if (!data) {
-    return (
-      <div className="alert alert-info shadow-lg">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="stroke-current flex-shrink-0 w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            ></path>
-          </svg>
-          <span>Loading NFTs...</span>
-        </div>
-      </div>
-    );
+    return <Status type="info" msg="Loading NFTs..." />
   }
 
   return <NFTPicker nfts={data} />
